eb1343f2e281bbf123cc8ca246600d8d9826f1cf
rahulprmr18 <119917030+rahulprmr18@users.noreply.github.com>
2024-03-17 10:42:01 +0000

    conv

--- 01_basics/03_conversionsOperations.js
+++ 01_basics/03_conversionsOperations.js
@@ -20,3 +20,46 @@ console.log(booleanIsLoggedIn);
 let someNuber = 33
 let stringNumber = String(someNuber)
 console.log( typeof stringNumber);
+
+
+//******************** OPRATIONS**********************/
+
+let value = 2
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**7);//power
+console.log(2/3);
+console.log(2%3);//remeder
+
+let str1 = "Rahul "
+let str2 = "Parmar"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log();
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log( (3 + 4) * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 =num3 = 2+2
+console.log(num1);
+
+let gameCounter = 100
+gameCounter ++
+console.log(gameCounter);
+
+
+
